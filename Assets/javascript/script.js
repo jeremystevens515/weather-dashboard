429e6b235415787f662a9bf977dd6169014e2163
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-01-11 11:09:25 -0600

    added code to store previous searches in local storage and got function to search from history working

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,25 +1,79 @@
 // variables
 var APIkey = '&appid=47321296effd62eab8d0754b0a9e9a55'
 var openWeatherAPI ='https://api.openweathermap.org/'
-// var geoCodingAPI = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityname + '&limit=5&appid=47321296effd62eab8d0754b0a9e9a55';
-// var currentWeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=47321296effd62eab8d0754b0a9e9a55';
-// var fiveDayForecast = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=47321296effd62eab8d0754b0a9e9a55';
+var previousSearches = [];
 
 // DOM selection
+var cityNameEl = document.querySelector('#city-name');
+var lastUpdated = document.querySelector('#last-updated');
 var searchCitiesInputEl = document.querySelector('#search-cities');
 var searchButtonEl = document.querySelector('#search-button');
+var searchHistoryEl = document.querySelector('#search-history')
+var currentCityOverview = document.querySelector('#current-city-overview'); // selector not working with id
+var currentTemp = document.querySelector('#current-temp');
+var currentWind = document.querySelector('#current-wind');
+var currentHumidity = document.querySelector('#current-humidity');
+
+// var fiveDayContainer = document.querySelector('#five-days');
 
 // get search value
 function getSearchValue(event) {
     event.preventDefault();
     var city = searchCitiesInputEl.value
     console.log(city);
+    cityNameEl.textContent = city;
+    lastUpdated.textContent = dayjs().format('hh:mm a');
+
+    clearCards();
+    addToHistory(city);
     getGeoCode(city)
 }
 
+function clearCards() {
+    var fiveDayContainer = document.querySelector('#five-days');
+    var child = fiveDayContainer.lastElementChild
+    while (child) {
+        fiveDayContainer.removeChild(child);
+        child = fiveDayContainer.lastElementChild
+    }
+}
+
+function addToHistory(textInput) {
+    previousSearches.push(textInput);
+    localStorage.setItem('previousSearches', JSON.stringify(previousSearches));
+    console.log(previousSearches);
+
+    var historyBtn = document.createElement('button');
+    historyBtn.setAttribute('class', 'btn text-secondary text-start');
+    historyBtn.textContent = textInput;
+    searchHistoryEl.appendChild(historyBtn);
+
+}
+
+function displayHistory() {
+    var getHistory = JSON.parse(localStorage.getItem('previousSearches'));
+    console.log(getHistory);
+    
+        for (var i = 0; i < getHistory.length; i++) {
+            var historyBtn = document.createElement('button');
+            historyBtn.setAttribute('class', 'btn text-secondary text-start');
+            console.log(getHistory[i])
+            historyBtn.textContent = getHistory[i];
+            searchHistoryEl.appendChild(historyBtn);
+        }
+}
+
+function searchFromHistory(event) {
+    console.log(event.target.innerHTML);
+    var city = event.target.innerHTML;
+    cityNameEl.textContent = city;
+    clearCards();
+    getGeoCode(city);
+}
+
 // get latitude and longitude based on city name
-function getGeoCode(cityname) {
-    var geoCodingURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityname + '&limit=5&appid=47321296effd62eab8d0754b0a9e9a55'
+function getGeoCode(cityName) {
+    var geoCodingURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=47321296effd62eab8d0754b0a9e9a55'
     fetch(geoCodingURL)
         .then(function(response) {
             return response.json();
@@ -30,14 +84,12 @@ function getGeoCode(cityname) {
             var longitude = data[0].lon;
             console.log('longitude ' + longitude);
 
-            // TODO: add search history
-
-            getCurrentConditions(latitude,longitude);
+            getCurrentConditions(cityName, latitude,longitude);
             getFiveDayForecast(latitude,longitude);
         });
 }
 
-function getCurrentConditions(lat,lon) {
+function getCurrentConditions(city,lat,lon) {
     var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=47321296effd62eab8d0754b0a9e9a55&units=imperial'
     fetch(currentWeatherURL)
         .then(function(response) {
@@ -45,19 +97,22 @@ function getCurrentConditions(lat,lon) {
         }).then(function(data) {
             console.log('Current Conditions \n-----------')
             console.log(data);
-            var temp = data.main.temp;
-            console.log('temp is ' + temp);
-            var wind = data.wind.speed;
-            console.log('wind speed is ' + wind + ' mph');
-            var humidity = data.main.humidity;
-            console.log('humidity is ' + humidity + ' %');
+            var description = data.weather[0].main;
+            var temp = data.main.temp + '℉';
+            var wind = data.wind.speed + ' mph';
+            var humidity = data.main.humidity + '%';
 
             // TODO: populate html elements
+            currentCityOverview.textContent = 'Currently, ' + description + ' in ' + city;
+            currentTemp.textContent = 'Temp: ' + temp; 
+            currentWind.textContent = 'Wind: ' + wind;
+            currentHumidity.textContent = 'Humidity: ' + humidity;
 
         });
 }
 
 function getFiveDayForecast(lat,lon) {
+
     var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=47321296effd62eab8d0754b0a9e9a55&units=imperial'
     fetch(fiveDayURL)
         .then(function(response) {
@@ -69,9 +124,9 @@ function getFiveDayForecast(lat,lon) {
                 console.log(data.list[i]);
                 var date = dayjs(data.list[i].dt_txt).format('M/D/YYYY');
                 console.log(date);
-                var temp = data.list[i].main.temp + ' &#8457';
-                var wind = data.list[i].wind.speed + ' Mph';
-                var humidity = data.list[i].main.humidity + ' %';
+                var temp = data.list[i].main.temp + '℉';
+                var wind = data.list[i].wind.speed + ' mph';
+                var humidity = data.list[i].main.humidity + '%';
                 createCard(date,temp,wind,humidity);
             }
         })
@@ -92,15 +147,15 @@ function createCard(date,temp,wind,humidity) {
     
     var predictedTemp = document.createElement('p');
     predictedTemp.setAttribute('class', 'card-text');
-    predictedTemp.textContent = temp;
+    predictedTemp.textContent = 'Temp: ' + temp;
 
     var predictedWind = document.createElement('p');
     predictedWind.setAttribute('class', 'card-text');
-    predictedWind.textContent = wind;
+    predictedWind.textContent = 'Wind: ' + wind;
 
     var predictedHumidity = document.createElement('p');
     predictedHumidity.setAttribute('class', 'card-text');
-    predictedHumidity.textContent = humidity;
+    predictedHumidity.textContent = 'Humidity: ' + humidity;
 
     predictedConditions.appendChild(predictedTemp);
     predictedConditions.appendChild(predictedWind);
@@ -112,8 +167,11 @@ function createCard(date,temp,wind,humidity) {
     divContainer.appendChild(divCardBody);
 
     // TODO: figure out why this selector does not work with custom attributes
-    var fiveDayContainer = document.querySelector('.card-group');
+    var fiveDayContainer = document.querySelector('#five-days');
     fiveDayContainer.appendChild(divContainer);
 }
 
+
 searchButtonEl.addEventListener('click', getSearchValue);
+searchHistoryEl.addEventListener('click', searchFromHistory);
+displayHistory();
